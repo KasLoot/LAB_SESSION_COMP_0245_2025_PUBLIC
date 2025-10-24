5c701780e123cc2ddec9ea41f1d0d289e980f824
Yuxin-LP <slyx361@outlook.com>
2025-10-24 15:29:26 +0100

    change part 1 mlp model layernorm to RMS norm

--- final/part_1/model.py
+++ final/part_1/model.py
@@ -4,12 +4,20 @@ import torch.optim as optim
 import torch.nn.functional as F
 from dataclasses import dataclass
 
+@dataclass
+class ModelConfig:
+    input_size: int = 7
+    hidden_size: int = 64
+    output_size: int = 7
+    num_heads: int = 4
+    num_layers: int = 2
+
 
 
 class Part_1_Model(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Part_1_Model, self).__init__()
-        self.rms1 = nn.LayerNorm(input_size)
+        self.rms1 = nn.RMSNorm(input_size)
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
@@ -20,3 +28,16 @@ class Part_1_Model(nn.Module):
         x = F.silu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+class Part_1_Transformer_Model(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_heads=4, num_layers=2):
+        super(Part_1_Transformer_Model, self).__init__()
+        self.input_proj = nn.Linear(input_size, hidden_size)
+        
+
+    def forward(self, x):
+        
+        return x
+    
+
